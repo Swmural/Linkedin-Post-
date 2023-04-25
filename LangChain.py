378be18535933aca8d145935e56fb0f3cd80afef
Swathi Muralinathan <114114508+Swmural@users.noreply.github.com>
2023-04-25 01:13:50 -0400

    Update LangChain.py

--- LangChain.py
+++ LangChain.py
@@ -1,59 +1,57 @@
 import os
 
-from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
-import requests
+from langchain.prompts import PromptTemplate
+import openai
 
-# The OpenAI API can be applied to virtually any task that involves understanding or generating natural language, code, or images
 
-# Steps to get the OpenAI API Key
+from flask import Flask, Response, request, jsonify, json
 
-# 1. Go to the OpenAI website and sign up for an account if you haven't already.
-# 2. Navigate to the "API Keys" tab in your OpenAI dashboard.
-# 3. Click on the "Generate New Key" button.
-# 4. Give your API key a name and choose the permissions you want to grant.
-# 5. Click on the "Generate API Key" button.
-# 6. Your new API key will be displayed on the screen. Make sure to copy it and store it
-# 7. Update your card details, only then your API KEY will work
+from dotenv import load_dotenv
 
-os.environ["OPENAI_API_KEY"] = "API-Key"
+load_dotenv()
 
-# This is where we get the input from the user
-apiInput = input("Please give the Input to generate a LinkedIn post with image? ")
+app = Flask(__name__)
 
-# Large Language Model (LLM) AI is a term that refers to AI models that can generate natural language texts from large amounts of data.
-llm = OpenAI(temperature=0.9)
-prompt = PromptTemplate(
+@app.route('/text_linkedIn_generator', methods=['GET'])
+def text_to_image_converter():
+  data = request.json
+  
+  if data is None or data == {}:
+    return Response(response=json.dumps({"Error": "Please provide post information"}),
+                        status=400,
+                        mimetype='application/json')
+  OpenAI.api_key = os.getenv('OPENAI_API_KEY')
+  
+  api_input = data['text']
+  image_size = data['size']
+
+  response = {}
+  
+  llm = OpenAI(temperature=0.1)
+
+  prompt = PromptTemplate(
     input_variables=["topic"],
-    template="Generate a LinkedIn post about {topic}",
-)
+    template="Write a LinkedIn post about {topic} with an image of size 512 x 512",
+    )
+  
+  chain = LLMChain(llm=llm, prompt=prompt)
 
-# The LLMChain is a simple chain that takes in a prompt template, formats it with the user input and returns the response from an LLM
-chain = LLMChain(llm=llm, prompt=prompt)
+  
+  llm_output = chain.run(api_input)
+  
+  response = openai.Image.create(
+    prompt= llm_output,
+    n=1,
+    size= image_size,
+  )
+  
+  response.data[0]['content'] = llm_output
+  
+  return Response(response=json.dumps(response),
+                    status=200,
+                    mimetype='application/json')
 
-# Calling OpenAI's DALL-E API with required input parameters to get the image url
-response = requests.post(
-    "https://api.openai.com/v1/images/generations",
-    headers={"Authorization": "Bearer API-Key"},
-    json={
-        "model": "image-alpha-001",
-        "prompt": "Random picture of a" + apiInput,
-        "num_images": 1,
-        "size": "512x512",
-        "response_format": "url"
-    }
-)
-
-# Generate a LinkedIn post
-# 1. We run the chain here with the input
-# 2. From the response, we get the image url
-# 3. And then, just combined it, so that it looks like a linkedIn post!
-print(chain.run(apiInput) + "\n" + response.json()["data"][0]["url"])
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True, port=5001, host='0.0.0.0')
